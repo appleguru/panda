876a9237c9e1a40cb29a276039da084e132b3a5e
Adam Urban <appleguru@gmail.com>
2018-08-04 17:56:24 -0400

    Adding some new CAN hooks for output

--- board/safety/safety_tesla.h
+++ board/safety/safety_tesla.h
@@ -87,20 +87,59 @@ static void tesla_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
   }
 
-  //Check if car is in reverse for camera switcher  
+  //Set UJA1023 outputs for camera swicther/etc.
+  
+  //0x118 is DI_torque2
   if (addr == 0x118) {
-    int drive_state = (to_push->RDLR >> 12) & 0x7;
-    
-    //if the car goes into reverse, set UJA1023 output pin 0 to high:
+    int drive_state = (to_push->RDLR >> 12) & 0x7; //DI_gear : 12|3@1+
+    int brake_pressed = (to_push->RDLR & 0x8000) >> 15;
+
+    //if the car goes into reverse, set UJA1023 output pin 0 to high. If Drive, set pin 1 high.
+    //DI_gear 7 "DI_GEAR_SNA" 4 "DI_GEAR_D" 3 "DI_GEAR_N" 2 "DI_GEAR_R" 1 "DI_GEAR_P" 0 "DI_GEAR_INVALID" ;
     if (drive_state == 2) {
       set_uja1023_output_bits(1 << 0);
       //puts("Got Reverse\n");
     } else {
       clear_uja1023_output_bits(1 << 0);
+    }
+    if (drive_state == 3) {
+      set_uja1023_output_bits(1 << 1);
       //puts("Got Drive\n");
+    } else {
+      clear_uja1023_output_bits(1 << 1);
+    }
+
+    //if the car's brake is pressed, set pin 2 to high
+    if (brake_pressed == 1) {
+      set_uja1023_output_bits(1 << 2);
+      //puts("Brake on!\n");
+    } else {
+      clear_uja1023_output_bits(1 << 2);
     }
   }
-  
+
+  //0x45 is STW_ACTN_RQ
+  if (addr == 0x45) {
+    int turnSignalLever = (to_push->RDLR & 0x30000); //TurnIndLvr_Stat : 16|2@1+
+    
+    //TurnIndLvr_Stat 3 "SNA" 2 "RIGHT" 1 "LEFT" 0 "IDLE" ;
+    if (turnSignalLever == 1) {
+      //Left turn signal is on, turn on output pin 3
+      set_uja1023_output_bits(1 << 3);
+      //puts("Left turn on!\n");
+    }
+    else {
+      clear_uja1023_output_bits(1 << 3);
+    }
+    if (turnSignalLever == 2) {
+      //Right turn signal is on, turn on output pin 4
+      set_uja1023_output_bits(1 << 4);
+      //puts("Right turn on!\n");
+    }
+    else {
+      clear_uja1023_output_bits(1 << 4);
+    }
+  }
 }
 
 // all commands: gas/regen, friction brake and steering
