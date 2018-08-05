1bc3a1a572f4afc08cef0881ae37a39c6999e3fd
Adam Urban <appleguru@gmail.com>
2018-08-04 20:10:10 -0400

    Pull in tesla_devel safety changes

--- board/safety/safety_tesla.h
+++ board/safety/safety_tesla.h
@@ -23,8 +23,9 @@ int eac_status = 0;
 int tesla_ignition_started = 0;
 
 static void tesla_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
-  enable_gmlan_switch(); //we're still in tesla safety mode, reset the timeout counter and make sure our output is enabled
-  
+  set_gmlan_digital_output(GMLAN_HIGH);
+  reset_gmlan_switch_timeout(); //we're still in tesla safety mode, reset the timeout counter and make sure our output is enabled
+
   //int bus_number = (to_push->RDTR >> 4) & 0xFF;
   uint32_t addr;
   if (to_push->RIR & 4) {
@@ -178,7 +179,7 @@ static int tesla_tx_lin_hook(int lin_num, uint8_t *data, int len) {
 static void tesla_init(int16_t param) {
   controls_allowed = 0;
   tesla_ignition_started = 0;
-  gmlan_switch_init();
+  gmlan_switch_init(1); //init the gmlan switch with 1s timeout enabled
   uja1023_init();
 }
 
